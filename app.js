eea5e4e7469c339b08288b729f4da491fed2e369
dyllanhiggins7@gmail.com <dyllanhiggins7@gmail.com>
2023-01-15 06:50:54 +0200

    Created (Dino & Human) objects and added methods to pull data from server,making use of a callback function.

--- app.js
+++ app.js
@@ -1,18 +1,56 @@
 
     // Create Dino Constructor
-    function Dino(species, weight, height, height, where, when, fact){
+    function Organism(species, weight, height, diet, where, when, fact)
+    {
         this.species = species,
         this.weight = weight,
         this.height = height,
+        this.diet = diet,
         this.where = where,
         this.when = when,
         this.fact = fact
     }
 
     // Create Dino Objects
-    
+    let dinos = [];
+    function Dino(species, weight, height, diet, where, when, fact)
+    {
+        Organism.call({dinos},species, weight, height, diet, where, when, fact)
+    }
+    Dino.prototype = Object.create(Organism.prototype);
+    Dino.prototype.constructor = Dino;    
+
+    //fetch dino data from server
+    const fetchDinoData = async () => {
+        const res = await fetch('https://dino-infographics.herokuapp.com/Dinos');
+        const data = await res.json();
+        return data;
+        };
+
+        const populateDino = async () => {
+            dinos = await fetchDinoData();
+            dinos.map(dino => new Dino(
+                    dino.species,
+                    dino.weight,
+                    dino.height,
+                    dino.diet,
+                    dino.where,
+                    dino.when,
+                    dino.fact,
+                ));
+
+            console.log(dinos);
+        }
+        populateDino();
+
 
     // Create Human Object
+    function human(name, species, weight, height, diet, where){
+        Organism.call({}, species, weight, height, diet, where);
+        this.name = name;
+    }
+    human.prototype = Object.create(Organism.prototype);
+    human.prototype.constructor = human;
 
     // Use IIFE to get human data from form
 
